b8ad7bc583540c0a86cb3749f715262e67fdf997
Jean Reis <jeanpreis@gmail.com>
2015-11-13 16:59:23 -0200

    version 0.15.10.13
    beginning of the project
    - functions to calculate the position of headers dynamically

---
+++ smartScroll.js
@@ -0,0 +1,46 @@
+function adjustTableHeader(element) {
+    //add tweaks here.
+    var width = element.closest('.container').outerWidth();
+    var left = element.closest('.container').offset().left;
+    element.width(width);
+    element.css('left', left);
+}
+
+$('.container').bind("scroll", function () {
+    $('header').each(function (idx) {
+        var originalTop = $(this).closest('.item-container').position().top
+        var containerScrollTop = $(this).scrollTop();
+        var noHeaderHeight = $(this).parent().siblings('article').outerHeight();
+        var nextPosition;
+        var height;
+        var stopPosition;
+
+        if ($('header').length > 1 && idx + 1 <= $('header').length - 1) {
+            nextPosition = $('header').eq(idx + 1).closest('.item-container').position().top - $('header').eq(idx + 1).height();
+            height = $('header').parent().outerHeight();
+        }
+
+        if (originalTop <= containerScrollTop) {
+            $(this).addClass('header-fixer');
+            adjustTableHeader($(this));
+
+            if (nextPosition) {
+                stopPosition = nextPosition - noHeaderHeight - height;
+                if (stopPosition < 0) {
+                    $(this).css('top', stopPosition);
+                    $('header').eq(idx + 1).addClass('header-fixer');
+
+                    if (nextPosition <= height) {
+                        $(this).removeClass('header-fixer');
+                    }
+                } else {
+                    $(this).css('top', 0);
+                }
+            }
+
+        } else {
+            $(this).removeClass('header-fixer');
+            $(this).removeAttr('style');
+        }
+    });
+});
